eab9d997554dedc38268e398e2adcc9c76b450fe
ruanpato <ruanpato@outlook.com>
2019-12-12 19:01:13 -0300

    Stable version

--- script.js
+++ script.js
@@ -7,26 +7,56 @@ import { GLTFLoader } from "https://threejsfundamentals.org/threejs/resources/th
 //import { SkeletonUtils } from "https://threejsfundamentals.org/threejs/resources/threejs/r110/examples//jsm/utils/SkeletonUtils.js";
 import { OrbitControls } from "https://threejsfundamentals.org/threejs/resources/threejs/r110/examples/jsm/controls/OrbitControls.js";
 
-var pickle, charizard;
-var container, stats, clock, gui, mixer, actions, activeAction, previousAction, controls;
-var camera, scene, renderer, charizard, face, light;
+var pickle, mew, fatPsyduck;
+var container, clock, gui, mixer, actions, activeAction, controls, rayCaster;
+var camera, scene, renderer, mew, light;
 
-var cameraOrbitVision = {charizard: true, pickle: false};
-var api = {Action: 'Take 01'};
+var cameraOrbitVision = {mew: true, pickle: false};
+var api = {Action: 'Static Pose'};
 var pickleRoll = {RollX: false, RollY: false, RollZ: false}, rotation = {RollX: 0.0, RollY: 0.0, RollZ: 0.0};
+
+const KEY ={
+    W: 87,
+    A: 65,
+    S: 83,
+    D: 68
+}
+
 
 init();
 animate();
+/*
+function ondDocumentKeyDown(event){
+    var keyCode = event.keyCode;
+    var lookAtVector = new THREE.Vector3(0,0, -1);
+    lookAtVector.applyQuaternion(camera.quaternion);
+    console.log(lookAtVector)
+    switch(keyCode){
+        case KEY.A:
+            if(lookAtVector['x'] > 0)
+                lookAtVector['x'] -= 1
+            break;
+        case KEY.D:
+            break;
+        case KEY.S:
+            break;
+        case KEY.W:
+            break;
+    }
+}*/
+
 function init() {
     // Container "config"
     container = document.createElement('div');
     document.body.appendChild(container);
     //document.addEventListener('keydown', ondDocumentKeyDown, false);
 
+    // To get clicked position
+    rayCaster = new THREE.Raycaster();
     // Camera "config"
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
     camera.position.set(70, 40, 70);
-//    camera.lookAt(new THREE.Vector3(0, 2, 0));
+    camera.lookAt(new THREE.Vector3(0, 10, 0));
 
     // Scene "config"
     scene = new THREE.Scene();
@@ -65,19 +95,8 @@ function init() {
     grid.material.transparent = true;
     scene.add( grid );
     */
-    // load Charizard model
+    // load mew model
     loader = new GLTFLoader();
-    loader.load('models/charizard/scene.glb', function ( gltf ) {
-        // Adding on scene object.scene
-        charizard = gltf
-        charizard.scene.position.set(-10, 0, -10);
-        charizard.scene.scale.set(0.4, 0.4, 0.4);
-        scene.add(charizard.scene);
-        // Create GUI
-        createGUI(charizard.scene, charizard.animations);
-    }, undefined, function ( e ) {
-        console.error( e );
-    } );
     // Load pickle rick
     loader.load('models/pickle_rick/scene.glb', function ( gltf ) {
         pickle = gltf
@@ -85,6 +104,27 @@ function init() {
         pickle.scene.position.set(35, 10, 0);
         pickle.scene.scale.set(1, 1, 1);
         scene.add(pickle.scene);
+    }, undefined, function ( e ) {
+        console.error( e );
+    } );
+    // Load fat Psyduck
+    loader.load('models/fatPsyduck/scene.glb', function ( gltf ) {
+        fatPsyduck = gltf
+        // Adding on scene object.scene
+        fatPsyduck.scene.position.set(75, 0, 0);
+        fatPsyduck.scene.scale.set(10, 10, 10);
+        scene.add(fatPsyduck.scene);
+    }, undefined, function ( e ) {
+        console.error( e );
+    } );
+    loader.load('models/mew/scene.glb', function ( gltf ) {
+        // Adding on scene object.scene
+        mew = gltf
+        mew.scene.position.set(0, 10, 0);
+        mew.scene.scale.set(0.08, 0.08, 0.08);
+        scene.add(mew.scene);
+        // Create GUI
+        createGUI(mew.scene, mew.animations);
     }, undefined, function ( e ) {
         console.error( e );
     } );
@@ -96,6 +136,7 @@ function init() {
     renderer.setSize( window.innerWidth, window.innerHeight );
     renderer.gammaOutput = true;
     renderer.gammaFactor = 2.2;
+
 
     // Controls
     controls = new OrbitControls(camera, renderer.domElement);
@@ -114,41 +155,38 @@ function createGUI(model, animations){
     gui = new GUI();
     mixer = new THREE.AnimationMixer(model);
     actions = {}
-    var actionsNames = ['Stopped', 'Take 01']
+    var actionsNames = ['Static Pose'];
     for(var i = 0; i < animations.length; i++){
         var clip = animations[i];
         var action = mixer.clipAction(clip);
-        actions[clip.name] = action;
-        actions[i] = clip.name;
+        
+        if(clip.name != 'metarig|SunAction'){
+            actionsNames[i+1] = clip.name;
+            actions[clip.name] = action;
+        }
+        
     }
     action.clampWhenFinished = true;
     action.loop = THREE.LoopOnce;
 
-    var actionsGUI = gui.addFolder('Charizard Actions');
-    var actionSelect = actionsGUI.add(api, 'Action').options(actionsNames)
-    
+    var actionsGUI = gui.addFolder('mew Actions');
+    var actionSelect = actionsGUI.add(api, 'Action').options(actionsNames);
     actionSelect.onChange(function(){
-
-        activeAction = actions[api['Action']]
-        if(activeAction != actions['Take 01'])
-            actions['Take 01'].stop();
-        else
-            activeAction.play()
+        if(api['Action'] == 'Static Pose'){
+            for(var key in actions){
+                activeAction = actions[key];
+                activeAction.stop();
+            }
+        }else{
+            actions[api['Action']].play()
+        }
     });
-
-    actionsGUI.open();
-
-    activeAction = actions['Take 01'];
-    if(activeAction != actions['Take 01'])
-        activeAction.stop();
-    activeAction.play();
-
+    
     // pickle
     actionsGUI = gui.addFolder('Pickle Roll');
     activeAction = actionsGUI.add(pickleRoll, 'RollX');
     activeAction = actionsGUI.add(pickleRoll, 'RollY');
     activeAction = actionsGUI.add(pickleRoll, 'RollZ');
-
 }
 
 //
@@ -162,15 +200,16 @@ function onWindowResize() {
 function animate() {
     // Pickle rotation
     if(pickleRoll['RollX'] == true)
-        pickle.scene.rotateX( (rotation['RollX'] += 0.0001) )
+        pickle.scene.rotateX( 0.1 )
     if(pickleRoll['RollY'] == true)
-        pickle.scene.rotateY( (rotation['RollY'] += 0.0001) )
+        pickle.scene.rotateY( 0.1 )
     if(pickleRoll['RollZ'] == true)
-        pickle.scene.rotateZ( (rotation['RollZ'] += 0.0001) )
+        pickle.scene.rotateZ( 0.1 )
 
     var deltaSeconds = clock.getDelta();
     if ( mixer ) mixer.update( deltaSeconds );
-    controls.update(deltaSeconds)
+    controls.update(deltaSeconds);
     requestAnimationFrame( animate );
+    
     renderer.render( scene, camera );
 }
